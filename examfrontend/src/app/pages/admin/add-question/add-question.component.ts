1ab66be41f7a204332990df216f5ffc5e14ca3bd
007ravi <mishra007ravi@gmail.com>
2022-08-07 22:27:54 +0530

    #32.2 add title field in question component to understand where question are adding

--- examfrontend/src/app/pages/admin/add-question/add-question.component.ts
+++ examfrontend/src/app/pages/admin/add-question/add-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-question',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddQuestionComponent implements OnInit {
 
-  constructor() { }
+  qId:any;
+  qTitle:any;
+  question={
+    quiz:{
+      qId:''
+    },
+    content:'',
+    option1:'',
+    option2:'',
+    option3:'',
+    option4:'',
+    answer:'',
+  };
+  constructor(
+    private _route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.qId=this._route.snapshot.params.qid;
+    this.qTitle=this._route.snapshot.params.title;
+    this.question.quiz['qId']=this.qId;
   }
 
 }
