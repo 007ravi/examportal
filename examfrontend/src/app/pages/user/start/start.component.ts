570fc242ec10273f155252326f4c7eaa0a553a8b
007ravi <mishra007ravi@gmail.com>
2022-08-14 01:25:15 +0530

    #41.2 prevent backbutton added

--- examfrontend/src/app/pages/user/start/start.component.ts
+++ examfrontend/src/app/pages/user/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  locationst:LocationStrategy) { }
 
   ngOnInit(): void {
+    this.preventBackButton();
   }
 
+
+  preventBackButton(){
+    history.pushState(null,"",location.href);
+    this.locationst.onPopState(()=>{
+      history.pushState(null,"",location.href);
+    })
+  }
 }
