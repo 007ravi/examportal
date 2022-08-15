63a5366e580281e369a285944481231e03f4a35e
007ravi <mishra007ravi@gmail.com>
2022-08-16 00:04:40 +0530

    #42 Quiz result page all done

--- examfrontend/src/app/pages/user/start/start.component.ts
+++ examfrontend/src/app/pages/user/start/start.component.ts
@@ -13,6 +13,10 @@ export class StartComponent implements OnInit {
 
   quizId:any;
   questions:any;
+  marksGot=0;
+  correctAnswers=0;
+  attempted=0;
+  isSubmit=false;
   constructor(private  locationst:LocationStrategy,
     private _route:ActivatedRoute,
     private _questionService:QuestionService
@@ -36,10 +40,41 @@ export class StartComponent implements OnInit {
     this._questionService.getQuestionOfQuizForTest(this.quizId).subscribe(
       (data)=>{
         this.questions=data;
+        this.questions.forEach((q:any) => {
+          q['givenAnswer']='';
+          console.log(this.questions);
+        });
       },
       (err)=>{
         Swal.fire("Error!","Error in loading questions of quiz",'error');
       }
     )
+  }
+
+  submitQuiz(){
+    Swal.fire({
+      title:'Do you want to submit the quiz?',
+      showCancelButton:true,
+      confirmButtonText:'Submit',
+      icon:'info',
+      denyButtonText:''
+    }).then((e)=>{
+      if(e.isConfirmed){
+        this.isSubmit=true;
+        this.questions.forEach((q:any) => {
+          if(q.givenAnswer==q.answer){
+            this.correctAnswers++;
+           let marksSingle= this.questions[0].quiz.maxMarks/this.questions.length;
+           this.marksGot+=marksSingle;
+       //    console.log(this.questions);
+          }
+          if(q.givenAnswer.trim()!=''){
+            this.attempted++;
+          }
+
+        });
+      }
+
+    })
   }
 }
